14ab4c7f38ecc37f4e4f7b95a5d82f6c3ce2fd74
kevidg <60922096+kevidg@users.noreply.github.com>
2025-04-09 06:46:55 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/select.h>
 #include <getopt.h>
 #include <ctype.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 /* !!!!!!!!!!!!!!!!!!!!!!!!! */
 /*    DEFINE stuff here  
@@ -19,12 +21,14 @@ Maybe move to a header file??
 /* ~~~~~ Function Prototypes ~~~~~~
 /////////////////////////////////////*/
 int validate_ip(const char *in_addr);
-void run_server();
-void run_client();
+void run_server(); // The server side of network connnection, waits for client to connect
+void run_client(); // The client side of the network connection
+void comms_loop(int sock_fd); // The function accepts a socket file descriptor and maintains a communication loop
 
 
 /* ~~~~~~ Globals ~~~~~*/
 char *IP_ADDRESS = "127.0.0.1";
+//static char default_msg[] = "Establishing Connection ...";
 //static char default_msg[] = "Establishing Connection ...";
 
 
@@ -35,9 +39,9 @@ int main(int argc, char *argv[]){
     // Arg Parser
     int opt;
     char *user_in_addr = NULL;
-    while((opt = getopt(argc, argv, "is:")) != -1){
+    while((opt = getopt(argc, argv, "cs")) != -1){
         switch (opt){
-            case 'i':
+            case 'c':
                 run_client();
                 break;
             case 's':
@@ -45,13 +49,13 @@ int main(int argc, char *argv[]){
                 break;
             case '?':
             default:
-               fprintf(stderr, "Usage: %s -i <ip_address>\n", argv[0]);
+               fprintf(stderr, "Usage: %s -s <ip_address>\n", argv[0]);
                return -1;
         }
     } // End Arg Parser While loop
 
     //IP check
-
+    /*
     if(user_in_addr == NULL){
         fprintf(stderr, "!! Error: Please provide an IP Address.\n");
         fprintf(stderr, "Usage: %s -i <ip_address>\n", argv[0]);
@@ -61,18 +65,22 @@ int main(int argc, char *argv[]){
     if (validate_ip(user_in_addr)){
         printf("## Connecting to IP Address: %s\n", user_in_addr);
         run_client();
+        run_client();
     }
     else{
         fprintf(stderr, "!! Error: Invalid IP address");
     }
 
-    
+    */
 
 } 
 
 /* ---- End Main ----*/
 /********************************************************************/
 
+/************************/
+/* Begin run_server code*/
+/************************/
 /************************/
 /* Begin run_server code*/
 /************************/
@@ -83,9 +91,11 @@ void run_server(){
     int server_socket, client_socket;
     struct sockaddr_in server_address, client_address;
     
+    
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if(server_socket < 0){
+        perror("!! Socket creation failed");
         perror("!! Socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -97,11 +107,13 @@ void run_server(){
 
     if(bind( server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0){
         perror("!! bind failed");
+        perror("!! bind failed");
         exit(EXIT_FAILURE);
     }
 
     //Listen
     if(listen(server_socket, 1) < 0){
+        perror("!! Listener failure");
         perror("!! Listener failure");
         close(server_socket);
         exit(EXIT_FAILURE);
@@ -121,8 +133,14 @@ void run_server(){
     char buffer[BUFFER_SIZE];
     char msg[] = "What's the BLUF?";
     printf("!! Testing clear text\nSending ---> '%s'\n", msg);
+    char buffer[BUFFER_SIZE];
+    char msg[] = "What's the BLUF?";
+    printf("!! Testing clear text\nSending ---> '%s'\n", msg);
     int bytes_read = read(client_socket, buffer, BUFFER_SIZE);
     buffer[bytes_read] = '\0';
+    printf("[%s]: %s\n", IP_ADDRESS, buffer);
+
+    
     printf("[%s]: %s\n", IP_ADDRESS, buffer);
 
     
@@ -143,12 +161,12 @@ void run_client(){
     struct sockaddr_in server_address;
     char buffer[BUFFER_SIZE] = "It's all chicken feed.";
 
-    //create socket
+    
     if((cl_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
         perror("!! Socket Failure");
         exit(EXIT_FAILURE);
     }
-    // Try to connect to server
+    
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(SERVER_PORT);
     inet_pton(AF_INET, IP_ADDRESS, &server_address.sin_addr);
@@ -168,6 +186,26 @@ void run_client(){
 }
 /* End run_client()*/
 /*************************/
+
+/********************/
+/* Begin comms_loop()*/
+/********************/
+void comms_loop(int ne_socket){
+    char buffer[BUFFER_SIZE];
+    fd_set readfds; 
+    int max_fd;
+
+    if(ne_socket > STDIN_FILENO){
+        max_fd = ne_socket;
+    }else{
+        max_fd = STDIN_FILENO;
+    }
+
+    while(1){
+        FD_ZERO(&readfds);
+        FD_SET()
+    }
+}
 
 /***************** */
 /*Input Validation*/
@@ -202,6 +240,9 @@ int validate_ip(const char *in_addr){
     free(cp_in_addr);
 
     return (segments == 4);
+} 
+/* End validate_ip() ***/
+/***************************************** */
 } 
 /* End validate_ip() ***/
 /***************************************** */
