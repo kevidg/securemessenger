5a26116a0ed3beabebdd34b7662a72205f319922
kevidg <m_guerette@msn.com>
2025-04-11 17:50:23 -0400

    Revert "Merge branch 'encrypt' to bring main..."
    This reverts commit 57da0ce65e37408062b074c08d0f0787951a6004, reversing
    changes made to 1e9187e2d3c6fb5b6eb8c41c273e24f417c7f21b.

--- main.c
+++ main.c
@@ -1,4 +1,35 @@
-#include "secmsg.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <getopt.h>
+#include <ctype.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+/* !!!!!!!!!!!!!!!!!!!!!!!!! */
+/*    DEFINE stuff here  
+Maybe move to a header file??
+/*****************************/
+#define SERVER_PORT 31337
+#define BUFFER_SIZE 1024
+
+
+/* ~~~~~ Function Prototypes ~~~~~~
+/////////////////////////////////////*/
+int validate_ip(const char *in_addr);
+void run_server(); // The server side of network connnection, waits for client to connect
+void run_client(); // The client side of the network connection
+void comms_loop(int sock_fd); // The function accepts a socket file descriptor and maintains a communication loop
+
+
+/* ~~~~~~ Globals ~~~~~*/
+char *IP_ADDRESS = "127.0.0.1";
+//static char default_msg[] = "Establishing Connection ...";
+
 
 /*****************/
 /* Start Here */
@@ -7,10 +38,6 @@ int main(int argc, char *argv[]){
     // Arg Parser
     int opt;
     char *user_in_addr = NULL;
-
-    OpenSSL_add_all_algorithms();
-    //generate_aes_key_iv();
-
     while((opt = getopt(argc, argv, "cs")) != -1){
         switch (opt){
             case 'c':
@@ -52,7 +79,7 @@ int main(int argc, char *argv[]){
 
 
 /************************/
-/* Begin run_server() */
+/* Begin run_server code*/
 /************************/
 void run_server(){
     /************************/
@@ -60,7 +87,6 @@ void run_server(){
 
     int server_socket, client_socket;
     struct sockaddr_in server_address, client_address;
-    
     
     
 
@@ -86,12 +112,9 @@ void run_server(){
         close(server_socket);
         exit(EXIT_FAILURE);
     }
-    printf("Listening on port ---> %d\n", SERVER_PORT);
 
+    printf("Listening on port ---> %d\n", SERVER_PORT);
     socklen_t clientLen = sizeof(client_address);
-    /*The socklen_t type is used in socket programming to represent the size of a socket address structure. 
-    It is typically used in functions that deal with socket addresses, such as accept(), getsockname(), and
-    getpeername().*/
     client_socket = accept(server_socket, (struct sockaddr *)&client_address ,&clientLen);
     if(client_socket < 0){
         perror("Client Accept Failed");
@@ -99,13 +122,20 @@ void run_server(){
         exit(EXIT_FAILURE);
     } 
 
-    printf(":: Connected to [name]\n:: Type message || Type 'quit()' to exit.\n");
+    printf(":: Connection established - %s\n", inet_ntoa(client_address.sin_addr));
+
+    char buffer[BUFFER_SIZE];
+    char msg[] = "What's the BLUF?";
+    printf("!! Testing clear text\nSending ---> '%s'\n", msg);
     
+    int bytes_read = read(client_socket, buffer, BUFFER_SIZE);
+    buffer[bytes_read] = '\0';
+    printf("[%s]: %s\n", IP_ADDRESS, buffer);
+
+    printf(":: Connected to [%s] :: Type message || Type 'quit()' to exit.\n", IP_ADDRESS);
     comms_loop(client_socket);
 
     close(server_socket);
-     
-    return;
 
 }
 /* End run_server() */
@@ -117,7 +147,7 @@ void run_server(){
 void run_client(){
     int cl_sock;
     struct sockaddr_in server_address;
-    //char buffer[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE] = "It's all chicken feed.";
 
     
     if((cl_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
@@ -127,8 +157,6 @@ void run_client(){
     
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(SERVER_PORT);
-    // The following function takes a string(IP_ADDRESS) and converts to an IPv4 binary and stores it 
-    // in the sockaddr_in struct (server_address)
     inet_pton(AF_INET, IP_ADDRESS, &server_address.sin_addr);
 
     if(connect(cl_sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0){
@@ -137,159 +165,60 @@ void run_client(){
         exit(EXIT_FAILURE);
     }
 
-    printf(":: Connected to [name] :: Type message || Type 'quit()' to exit.\n");
+    send(cl_sock, buffer, strlen(buffer), 0);
+    printf("## Message to server ---> %s\n", buffer);
+    printf(":: Connected to [%s] :: Type message || Type 'quit()' to exit.\n");
     comms_loop(cl_sock);
     close(cl_sock);
-    return;
 }
 /* End run_client()*/
-/***************************************************************/
+/*************************/
 
 /********************/
 /* Begin comms_loop()*/
 /********************/
 void comms_loop(int ne_socket){
     char buffer[BUFFER_SIZE];
+    fd_set readfds; 
+    int max_fd;
 
-    //Create new thread for receiving messages
-    pthread_t receiver_thread;
-    pthread_create(&receiver_thread, NULL, msg_receiver, &ne_socket );
+    if(ne_socket > STDIN_FILENO){
+        max_fd = ne_socket;
+    }else{
+        max_fd = STDIN_FILENO;
+    }
 
     while(1){
-        memset(buffer, 0, BUFFER_SIZE); // clear buffer
-        fgets(buffer, BUFFER_SIZE, stdin); //Get input form user
-        buffer[strcspn(buffer, "\n")] = '\0'; // strip the '/n' from the end
+        FD_ZERO(&readfds);
+        FD_SET(ne_socket, &readfds); // monitors network
+        FD_SET(STDIN_FILENO, &readfds); // monitors keyboard, stdin
 
-        if(strcmp(buffer, "quit()") == 0){
-            unsigned char ciphertxt[BUFFER_SIZE];
-            int ciphertext_len = aes_encrypt((unsigned char*)buffer, strlen(buffer), ciphertxt);
-            send(ne_socket, ciphertxt, ciphertext_len, 0);
-            shutdown(ne_socket, SHUT_RDWR);
-            break;
-        }
-        unsigned char ciphertxt[BUFFER_SIZE];
-        int ciphertext_len = aes_encrypt((unsigned char*)buffer, strlen(buffer), ciphertxt);
-        send(ne_socket, ciphertxt, ciphertext_len, 0);
-    }
-    pthread_join(receiver_thread, NULL);
-    
-}
-/* End comms_loop() */
-/***************************************************************/
-
-/***************************/
-/*  Begin msg_receiver()  */
-/************************ */
-//When a connection is made a thread is created to receive the incoming messages, 
-//this function is called by the new thread. 
-
-void *msg_receiver(void *arg){
-    int sock = *(int *)arg; //the incoming socket from the function argument
-    char buffer[BUFFER_SIZE];
-
-    while(1){
-        memset(buffer, 0, BUFFER_SIZE); // Clears the buffer
-        int bytes_received = recv(sock, buffer, BUFFER_SIZE, 0); // reads the size of the incoming message
-        if(bytes_received <= 0){ // if no bytes come in the connection is closed
-            printf("!! Connection Lost\n");
+        if (select(max_fd + 1, &readfds, NULL, NULL, NULL) < 0){
+            perror("select error");
             break;
         }
 
-        unsigned char plaintext[BUFFER_SIZE];
-        int plaintext_len = aes_decrypt((unsigned char*)buffer, bytes_received, plaintext);
-        plaintext[plaintext_len] = '\0'; // Null-terminate
-        // debug
-       // printf("[DEBUG] Received plaintext: '%s'\n", plaintext);
-        // End debug
-        if(strcmp((char*)plaintext, "quit()") == 0){
-            printf(":: Chat ended.\n");
-            break;
+        //Check for user input
+        if(FD_ISSET(STDIN_FILENO, &readfds)){
+            fgets(buffer, sizeof(buffer), stdin);
+            if(strncmp(buffer, "quit()", 6) == 0){
+                break;
+            }
+            send(ne_socket, buffer, strlen(buffer), 0);
         }
-        printf("[Name]: %s\n", plaintext);
+
+        //Check for network data
+        if(FD_ISSET(ne_socket, &readfds)){
+            int bytes_read = recv(ne_socket, buffer, sizeof(buffer)-1, 0);
+            if(bytes_read <= 0){
+                printf("!! Closed Connection\n");
+                break;
+            }
+            buffer[bytes_read] = '\0';
+            printf("[%s]: %s", IP_ADDRESS, buffer);
+        }
     }
-    pthread_exit(NULL);
 }
-/* End msg_receiver() */
-/****************************************************************/
-
-/********************/
-/* Begin generate_aes_key_iv() */
-/*********************/
-void generate_aes_key_iv(){
-    RAND_bytes(aes_key, sizeof(aes_key));
-    RAND_bytes(aes_iv, sizeof(aes_iv));
-}
-/* End generate_aes_key_iv() */
-/*****************************************************************/
-
-/**********************/
-/* Begin aes_encrypt()*/
-/**********************/
-int aes_encrypt(const unsigned char *plaintxt, int plaintxt_len, unsigned char *ciphertxt){
-    EVP_CIPHER_CTX *ctx;
-    int len, ciphertxt_len;
-
-    // intialize the cipher context
-    if(!(ctx = EVP_CIPHER_CTX_new())){
-        perror("CTX not initialized");
-    }
-    // initialize the encryptiong operation
-    if(EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, aes_key, aes_iv) != 1){
-        perror("Encrypt Op not Initialized");
-    }
-    // Provide message to be encrypted
-    if(EVP_EncryptUpdate(ctx, ciphertxt, &len, plaintxt, plaintxt_len) != 1){
-        perror("Error encrypting message");
-    }
-    ciphertxt_len = len;
-    // Finalize encryption
-    if(EVP_EncryptFinal_ex(ctx, ciphertxt + len, &len) != 1){
-        perror("Error Finalizing encryption");
-    }
-    ciphertxt_len += len;
-
-    // Clean up
-    EVP_CIPHER_CTX_free(ctx);
-    return ciphertxt_len;
-
-}
-/* End aes_encrypt()*/
-/*****************************************************************************/
-
-/**********************/
-/* Begin aes_decrypt()*/
-/**********************/
-int aes_decrypt(const unsigned char *ciphertxt, int ciphertxt_len, unsigned char *plaintxt){
-    EVP_CIPHER_CTX *ctx;
-    int len, plaintxt_len;
-
-    // intialize the cipher context
-    if(!(ctx = EVP_CIPHER_CTX_new())){
-        perror("CTX not initialized");
-    }
-    // initialize the decryption operation
-    if(EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, aes_key, aes_iv) != 1){
-        perror("Decrypt Op not Initialized");
-    }
-    // Provide message to be decrypted
-    if(EVP_DecryptUpdate(ctx, plaintxt, &len, ciphertxt, ciphertxt_len) != 1){
-        perror("Error decrypting message");
-    }
-    plaintxt_len = len;
-
-    // Finalize encryption
-    if(EVP_DecryptFinal_ex(ctx, plaintxt + len, &len) != 1){
-        perror("Error Finalizing decryption");
-    }
-    plaintxt_len += len;
-
-    // Clean up
-    EVP_CIPHER_CTX_free(ctx);
-    return plaintxt_len;
-
-}
-/* End aes_decrypt()*/
-/*****************************************************************************/
 
 /***************** */
 /*Input Validation*/
@@ -326,4 +255,4 @@ int validate_ip(const char *in_addr){
     return (segments == 4);
 } 
 /* End validate_ip() ***/
-/***************************************************************/
+/***************************************** */
